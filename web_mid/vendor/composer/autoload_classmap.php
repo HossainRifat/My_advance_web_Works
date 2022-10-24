c4b0adef747b59aea8f7ab8dda331ed2e79f7b6e
HossainRifat <rh140025@gmail.com>
2022-10-24 12:27:02 +0600

    migrations

--- web_mid/vendor/composer/autoload_classmap.php
+++ web_mid/vendor/composer/autoload_classmap.php
@@ -32,6 +32,7 @@ return array(
     'App\\Http\\Middleware\\TrimStrings' => $baseDir . '/app/Http/Middleware/TrimStrings.php',
     'App\\Http\\Middleware\\TrustHosts' => $baseDir . '/app/Http/Middleware/TrustHosts.php',
     'App\\Http\\Middleware\\TrustProxies' => $baseDir . '/app/Http/Middleware/TrustProxies.php',
+    'App\\Http\\Middleware\\ValidReg02' => $baseDir . '/app/Http/Middleware/ValidReg02.php',
     'App\\Http\\Middleware\\ValidateSignature' => $baseDir . '/app/Http/Middleware/ValidateSignature.php',
     'App\\Http\\Middleware\\VerifyCsrfToken' => $baseDir . '/app/Http/Middleware/VerifyCsrfToken.php',
     'App\\Models\\User' => $baseDir . '/app/Models/User.php',
